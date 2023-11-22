c533999a8281345f45f7174d9040a1f22fe4062b
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-22 13:53:50 -0600

    02 of foundations Fundamentals Part 4 lesson down. 5 more to go ...

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+  let finalWord = "";
+
+  if (num >= 0){
+
+    for (let i=0; num > i; i++){
+      finalWord += string;
+    }
+
+    return finalWord;
+
+  } else {
+    return "ERROR";
+  }
+
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
