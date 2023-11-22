c25d5f42b682174f0438650f93e5d014dbb6be32
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-22 14:31:24 -0600

    03 of foundations Fundamentals Part 4 lesson down. 4 more to go ...

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+  let newWord=[];
+
+  for (let letter in word) {
+    // newWord.unshift(word.shift());
+    newWord.unshift(word[letter]);
+
+  };
+
+  return newWord.join("");
 
 };
+
+// console.log(reverseString('frame'));
 
 // Do not edit below this line
 module.exports = reverseString;
