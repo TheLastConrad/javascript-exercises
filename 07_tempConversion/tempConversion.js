c2dbeae03ffe79e7cb95af0fc438cfb060e9c8f6
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-23 00:18:43 -0600

    07 of foundations Fundamentals Part 4 lesson down. 0 more to go ...

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDegrees) {
+  return parseFloat(((fDegrees-32)*(5/9)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  return parseFloat(((celsiusDegrees * 1.8)+32).toFixed(1))
 };
 
 // Do not edit below this line
