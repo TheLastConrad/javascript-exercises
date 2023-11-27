6b4b146ab882d896788c26d8a73a87708aea607b
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-27 17:03:11 -0600

    10 down on Fundamentals 5 foundation lesson. 2 more to go!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,40 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+  let yourAnswer;
+  let fiboSum = [1];
+  let definitelyAGoodNumber = 0;
+
+  function ensureAGoodNumber(num){
+    if (typeof (parseInt(num)+1) == "number"){ //is it a number masquerading as a string? We are doing numbers
+      if (parseInt(num) > 0) { // is it negaive number? We don't do negatives here
+        return parseInt(num);
+      } else{
+        yourAnswer = "OOPS"; //if its not a good number, straight to the answer
+      }
+    } else {
+      yourAnswer = "OOPS"; //if its not a good number, straight to the answer
+    }
+  }
+
+  definitelyAGoodNumber = ensureAGoodNumber(number);
+
+  // if its not a fail as per the 'OOPS' string, then go fibonacci!
+  if (yourAnswer !== "OOPS") {
+    for (let i = 0; i < definitelyAGoodNumber; i ++){
+      if (i==0){
+        yourAnswer = fiboSum[i];
+        fiboSum.push(fiboSum[i]); 
+        // console.log(`[${i}] fiboSum: ${fiboSum} | yourAnswer: ${yourAnswer}`)
+      }else{
+        yourAnswer = fiboSum[i];
+        fiboSum.push(fiboSum[i-1]+fiboSum[i]);
+        // console.log(`[${i}] fiboSum: ${fiboSum} | yourAnswer: ${yourAnswer}`)
+      }
+        
+    }
+  } 
+
+  return yourAnswer;
 
 };
 
