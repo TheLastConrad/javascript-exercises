57ce4142b2bddf99a01a5064c91d17ca88fccc46
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-27 15:43:12 -0600

    09 down on Fundamentals 5 foundation lesson. 3 more to go!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  
+  let freshArg = word;
+
+  //take out caps and punctuation
+  let stringNoPunct = freshArg.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  
+  let pureString = stringNoPunct.replace(/[\s]/g,"");
+  
+
+  let origWord = pureString.toLowerCase();
+
+  let revWord = (Array.from(origWord)).reverse().join('');
+
+  
+  if (revWord === origWord) {
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
