a8dfdd0936409136195336312ebf30414de16052
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-27 12:56:23 -0600

    08 down on Fundamentals 5 foundation lesson. 4 more to go!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,60 @@
-const add = function() {
+const add = function(x,y) {
+  return x+y;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
 const sum = function() {
-	
+  let sum = 0;
+
+  let args = [...arguments].flat()
+
+	for (let i in args){
+    sum += parseInt(args[i]);
+  }
+
+  return sum;
 };
 
 const multiply = function() {
+  let product = 1;
+  let args = [...arguments].flat();
+  
+  console.log(args[0])
+  console.log(args[1])
+
+  for (let i=args.length-1; i >= 0; i--) {
+   
+    product *= args[i];
+    
+    // console.log(`pass ${i} , working with ${args[i]} ... current product: [${product}]`)
+    
+  }
+
+  return product;
 
 };
 
-const power = function() {
+
+const power = function(x,y) {
+  return x**y;
 	
 };
 
-const factorial = function() {
+const factorial = function(x) {
+
+  let fac = 1;
+
+  for (let i = x; i > 0 ; i--){
+
+    fac *= i;
+
+  }
+
+  return fac;
 	
 };
 
