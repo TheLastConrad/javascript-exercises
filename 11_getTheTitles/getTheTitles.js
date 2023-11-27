7e178391949e70c5592da08a40212db9401f19b1
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-27 17:49:45 -0600

    11 down on Fundamentals 5 foundation lesson. 1 more to go!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  let finalArray = [];
+  array.forEach((element) => finalArray.push(element.title));
+  return finalArray;
 };
 
 // Do not edit below this line
