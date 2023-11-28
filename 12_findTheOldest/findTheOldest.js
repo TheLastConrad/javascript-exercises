0b39d0beec0282eebe213fd5b23c1bc79a64e5e4
TheLastConrad <9260055+TheLastConrad@users.noreply.github.com>
2023-11-27 20:24:21 -0600

    12 should be down, as the function works in browser and several other things, but it doesn't work in npm test jest ... I need to compare this and the solution to see why it isn't the same...

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,72 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+const findTheOldest = function(arr) {
+
+  let arrCopy = arr;
+  // Get an Array of all the ages in order
+  lineUpAgesIntoAgeArray(arr);
+
+  // Get the lowest value of ageArray and output it
+  
+  for (let i=0; i < (ageArray.length-2); i++){   
+    if (ageArray[i] > ageArray[i+1]) {
+      oldestAge = ageArray[i];
+      oldestPerson = arrCopy[i]
+    } else if (ageArray[i] < ageArray[i+1]){
+      oldestAge = ageArray[i+1];
+      oldestPerson = arrCopy[i+1];
+    }
+
+    
+  }
+
+  return oldestPerson;
+
+
 
 };
+
+let ageArray = [];
+let oldestPerson = {};
+let oldestAge = 0;
+
+function lineUpAgesIntoAgeArray(arr){
+  arr.forEach(element => {
+    // console.log(`lineUpAgesIntoAgeArray: element(${element.name}) birth(${element.yearOfBirth}), death(${element.yearOfDeath}) `)
+    ageArray.push(calculateAge(element.yearOfBirth, element.yearOfDeath))
+    // console.log(ageArray.[element.name-1]);
+
+  
+  });
+
+
+}
+
+function calculateAge(birth, death){
+  if (death == undefined){ 
+    death = 2023
+    }
+      
+  return death - birth;
+};
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
